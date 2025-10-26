13c991bdff8cf10bcb9ca3f4080739308f5a1c75
Aidar <mamaturaimovaydar@gmail.com>
2025-10-26 23:20:07 +0000

    guess number

--- To-Do List.py
+++ To-Do List.py
@@ -1,19 +1,46 @@
 to_do = []
 
+while True:
+    print("\nTo-Do List")
+    print("1. Add a task")
+    print("2. View a task")
+    print("3. Delete a task")
+    print("4. Quit")
 
-user_decision = int(input('What do you want to do? '))
+    user_decision = input('What do you want to do (1-4)?: ')
 
-if user_decision == 1:
-    add_task = str(input('What task do you want to add? '))
-    to_do.append(add_task)
+    if user_decision == "1":
+        add_task = str(input('What task do you want to add? '))
+        if add_task:
+            to_do.append(add_task)
+            print("Task added")
+        else:
+            print("Task was not added")
 
-elif user_decision == 2:
-    print("This is all your tasks!", to_do)
+    elif user_decision == "2":
+        if not to_do:
+            print("No tasks in the list")
+        else:
+            print("\nThis is all your tasks: ", to_do)
 
-elif user_decision == 3:
-    delete_task = str(input('What task do you want to delete? '))
-    to_do.remove(delete_task)
+    elif user_decision == "3":
+        if not to_do:
+            print("No tasks to delete")
+        else:
+            print("\nYour tasks: ", to_do)
+
+        delete_task = str(input('Which task do you want to delete? '))
+        if delete_task in to_do:
+            to_do.remove(delete_task)
+            print("Task deleted")
+        else:
+            print("Task was not deleted")
+
+    elif user_decision == "4":
+        break
+
+    else:
+        print("Invalid input. Choose number from 1-4")
 
 
 
-print(to_do)
