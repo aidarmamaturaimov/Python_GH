3f573880155d02aa10bb251398032ddfb2f078f7
Aidar <mamaturaimovaydar@gmail.com>
2025-12-26 20:58:41 +0000

    bank_account

--- Week9/Bank_account_class.py
+++ Week9/Bank_account_class.py
@@ -5,4 +5,12 @@ def bank_account():
     print("\nBank Account")
     int user = 0
 
+def deposit():
+    print("\nDeposit")
+
+def withdraw():
+    print("\nWithdraw")
+
+def display_balance():
+    print("\nDisplay Balance")
 
